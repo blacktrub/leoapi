2cc06bf615c26664ac19b4fc0652230341d4c163
blacktrub <lightlocdog@gmail.com>
2017-10-27 01:14:45 +0300

    added simple lingualeo api wrapper

---
+++ api/constants.py
@@ -0,0 +1,9 @@
+import enum
+
+
+BASE_URL = 'http://api.lingualeo.com'
+
+
+class RequestMethod(enum.Enum):
+    GET = 'GET'
+    POST = 'POST'
---
+++ api/wrapper.py
@@ -0,0 +1,55 @@
+import requests
+from typing import Dict, AnyStr, List
+
+from .constants import RequestMethod, BASE_URL
+
+
+class LinguaLeoApi:
+    email = None
+    password = None
+    session = None
+
+    def __init__(self, email: AnyStr, password: AnyStr):
+        self.email = email
+        self.password = password
+        self.session = requests.session()
+
+        self.auth()
+
+    def auth(self) -> Dict:
+        data = {
+            'email': self.email,
+            'password': self.password,
+        }
+        return self.request(
+            path='/api/login',
+            method=RequestMethod.POST,
+            data=data,
+        )
+
+    def translate(self, word: AnyStr) -> List:
+        response = self.request(
+            path='/gettranslates',
+            method=RequestMethod.GET,
+            params={'word': word},
+        )
+        return [item['value'] for item in response['translate']]
+
+    def send_word(self, word: AnyStr, translate: AnyStr):
+        return self.request(
+            path='/addword',
+            method=RequestMethod.POST,
+            data={'word': word, 'tword': translate},
+        )
+
+    def request(self,
+                path: AnyStr,
+                method: RequestMethod,
+                **kwargs) -> Dict:
+        url = BASE_URL + path
+        response = self.session.request(
+            method=method.value,
+            url=url,
+            **kwargs
+        )
+        return response.json()
